eeb17b5153c34172180ab25c149142d068890a90
Riccardo Ressi <riccardo.ressi@gmail.com>
2021-06-29 19:10:17 +0200

    edit_distance::matchStrings: optimization for maxDistance=1

--- edit_distance/edit_distance.cpp
+++ edit_distance/edit_distance.cpp
@@ -6,14 +6,49 @@
 
 namespace edit_distance {
 
+bool matchStringsWithMaxEditDistanceOfOne(StringView s, StringView t);
 
 bool matchStrings(StringView s, StringView t, Distance maxDistance) {
 
-    return calculateEditDistance(s, t) <= maxDistance;
+    switch (maxDistance) {
+
+        case 0:
+            return s == t;
+
+        case 1:
+            return matchStringsWithMaxEditDistanceOfOne(s, t);
+
+        default:
+            return calculateEditDistance(s, t) <= maxDistance;
+
+    }
+}
+
+auto getStringSuffix(StringView s, std::size_t pos) -> StringView {
+    if (pos < s.size()) {
+        return s.substr(pos);
+    } else {
+        return {};
+    }
+}
+
+bool matchStringsWithMaxEditDistanceOfOne(StringView s, StringView t) {
+
+    auto i = 0;
+    while (i < s.size()
+           && i < t.size()
+           && s[i] == t[i]) {
+        i++;
+    }
+
+    return (i == s.size() && i == t.size())
+           || getStringSuffix(s, i + 1) == getStringSuffix(t, i)
+           || getStringSuffix(s, i) == getStringSuffix(t, i + 1)
+           || getStringSuffix(s, i + 1) == getStringSuffix(t,i + 1);
 }
 
 auto calculateEditDistance(StringView s, StringView t) -> Distance {
-    auto matrix = std::vector<std::vector<int>>();
+    auto matrix = std::vector<std::vector<int >>();
     matrix.resize(s.size() + 1);
 
     for (auto i = 0; i < matrix.size(); i++) {
