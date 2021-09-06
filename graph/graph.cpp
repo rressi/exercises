289ee546e29cd11162aaa9067acec94200860956
Riccardo Ressi <riccardo.ressi@gmail.com>
2021-09-06 04:46:26 +0200

    Implement exercize Graph

---
+++ graph/graph.cpp
@@ -0,0 +1,86 @@
+#include "graph.h"
+
+#include <algorithm>
+#include <deque>
+#include <set>
+
+namespace graph {
+
+using ColorQueue = std::deque<Color>;
+using EdgeQueue = std::deque<Edge>;
+using VisitedSet = std::set<Edge>;
+
+struct Runner {
+    ColorQueue colorQueue{};
+    EdgeQueue edgeQueue{};
+    VisitedSet visitedSet{};
+};
+
+bool has_path(const Graph &graph, const ColorList &colorList) {
+
+    if (colorList.empty()) {
+        return false;
+    }
+
+    auto firstColor = colorList.front();
+    if (colorList.size() == 1) {
+
+        auto findIt = std::find_if(graph.begin(),
+                                   graph.end(),
+                                   [firstColor](const Node &node) {
+                                       return node.color == firstColor;
+                                   });
+        return findIt != graph.end();
+    }
+
+    auto runners = std::vector<Runner>();
+    for (auto edge = Edge(0); edge < graph.size(); edge++) {
+        const auto &node = graph.at(edge);
+        if (node.color != firstColor) continue;
+        if (node.edges.empty()) continue;
+
+        runners.push_back(Runner{
+                .colorQueue = ColorQueue{colorList.begin() + 1,
+                                         colorList.end()},
+                .edgeQueue = EdgeQueue{node.edges.begin(),
+                                       node.edges.end()},
+                .visitedSet = VisitedSet{edge}
+        });
+    }
+
+    while (!runners.empty()) {
+
+        for (auto &runner: runners) {
+            auto edge = runner.edgeQueue.front();
+            runner.edgeQueue.pop_front();
+
+            auto targetColor = runner.colorQueue.front();
+
+            const auto &node = graph.at(edge);
+            if (node.color == targetColor) {
+                runner.colorQueue.pop_front();
+                if (runner.colorQueue.empty()) {
+                    return true;
+                }
+            }
+
+            runner.visitedSet.insert(edge);
+            for (auto nextEdge: node.edges) {
+                if (runner.visitedSet.count(nextEdge) == 0) {
+                    runner.edgeQueue.push_back(nextEdge);
+                }
+            }
+        }
+
+        auto it = std::remove_if(runners.begin(), runners.end(),
+                                 [](const Runner &runner) {
+                                     return runner.edgeQueue.empty();
+                                 });
+        runners.resize(std::distance(runners.begin(), it));
+    }
+
+    return false;
+}
+
+
+} // namespace concatenated_list
---
+++ graph/graph.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <deque>
+#include <vector>
+
+namespace graph {
+
+using Edge = int;
+using Edges = std::vector<Edge>;
+
+enum class Color {
+    Black, Blue, Green, Orange, Red, Yellow, White
+};
+
+struct Node {
+    Color color{};
+    std::vector<Edge> edges{};
+};
+using Graph = std::vector<Node>;
+
+using ColorList = std::vector<Color>;
+
+bool has_path(const Graph &graph, const ColorList &colorList);
+
+} // namespace concatenated_list
+
