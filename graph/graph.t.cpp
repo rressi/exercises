006e7a52a4ddc947d02ac1f6b2fc3c303da1abe0
Riccardo Ressi <riccardo.ressi@gmail.com>
2022-01-01 11:16:46 +0100

    graph: reimplemented as a class

--- graph/graph.t.cpp
+++ graph/graph.t.cpp
@@ -3,116 +3,155 @@
 
 #include "graph.h"
 
+
 namespace graph {
+namespace test {
+namespace {
+
+using Color = graph::Graph::Color;
+using ColorList = graph::Graph::ColorList;
+using NodeId = graph::Graph::NodeId;
+using Edges = std::vector<NodeId>;
+
+struct WithNode {
+    Color color{};
+    Edges edges{};
+};
+using WithGraph = std::vector<WithNode>;
+
+bool testHasPath(const WithGraph &withGraph, const ColorList &colorList) {
+    auto graph = Graph();
+    for (const auto &withNode : withGraph) {
+        graph.addNode(withNode.color);
+    }
+    auto sourceNodeId = Graph::NodeId();
+    for (const auto &withNode : withGraph) {
+        for (const auto &destinationNodeId : withNode.edges) {
+            graph.addEdge(sourceNodeId, destinationNodeId);
+        }
+        sourceNodeId++;
+    }
+
+    return graph.hasPath(colorList);
+}
+
+}  // namespace
 
 TEST(GraphTest, empty) {
-    EXPECT_FALSE(has_path(Graph{}, ColorList{}));
-    EXPECT_FALSE(has_path(Graph{}, ColorList{Color::Red, Color::Green}));
+    EXPECT_FALSE(testHasPath(WithGraph{}, ColorList{}));
+    EXPECT_FALSE(testHasPath(WithGraph{}, ColorList{Color::Red, Color::Green}));
 }
 
 TEST(GraphTest, oneItem) {
-    EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{0}}}, ColorList{Color::Red}));
-    EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{}}}, ColorList{Color::Red}));
-    EXPECT_FALSE(
-        has_path(Graph{Node{Color::Red, Edges{0}}}, ColorList{Color::Yellow}));
-    EXPECT_FALSE(has_path(Graph{Node{Color::Red, Edges{0}}},
-                          ColorList{Color::Red, Color::Yellow}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{0}}},
+                            ColorList{Color::Red}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{}}},
+                            ColorList{Color::Red}));
+    EXPECT_FALSE(testHasPath(WithGraph{WithNode{Color::Red, Edges{0}}},
+                             ColorList{Color::Yellow}));
+    EXPECT_FALSE(testHasPath(WithGraph{WithNode{Color::Red, Edges{0}}},
+                             ColorList{Color::Red, Color::Yellow}));
 }
 
 TEST(GraphTest, twoItems) {
-    EXPECT_TRUE(has_path(
-        Graph{Node{Color::Red, Edges{1}}, Node{Color::Yellow, Edges{0}}},
-        ColorList{Color::Red}));
-    EXPECT_TRUE(has_path(
-        Graph{Node{Color::Red, Edges{1}}, Node{Color::Yellow, Edges{0}}},
-        ColorList{Color::Red, Color::Yellow}));
-    EXPECT_TRUE(has_path(
-        Graph{Node{Color::Red, Edges{1}}, Node{Color::Yellow, Edges{0}}},
-        ColorList{Color::Yellow, Color::Red}));
-    EXPECT_FALSE(has_path(
-        Graph{Node{Color::Red, Edges{0}}, Node{Color::Yellow, Edges{1}}},
-        ColorList{Color::Yellow, Color::Red}));
-    EXPECT_FALSE(has_path(
-        Graph{Node{Color::Red, Edges{0}}, Node{Color::Yellow, Edges{}}},
-        ColorList{Color::Yellow, Color::Red}));
-    EXPECT_TRUE(has_path(
-        Graph{Node{Color::Red, Edges{0}}, Node{Color::Yellow, Edges{1}}},
-        ColorList{Color::Yellow}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Yellow, Edges{0}}},
+                            ColorList{Color::Red}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Yellow, Edges{0}}},
+                            ColorList{Color::Red, Color::Yellow}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Yellow, Edges{0}}},
+                            ColorList{Color::Yellow, Color::Red}));
+    EXPECT_FALSE(testHasPath(WithGraph{WithNode{Color::Red, Edges{0}},
+                                       WithNode{Color::Yellow, Edges{1}}},
+                             ColorList{Color::Yellow, Color::Red}));
+    EXPECT_FALSE(testHasPath(WithGraph{WithNode{Color::Red, Edges{0}},
+                                       WithNode{Color::Yellow, Edges{}}},
+                             ColorList{Color::Yellow, Color::Red}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{0}},
+                                      WithNode{Color::Yellow, Edges{1}}},
+                            ColorList{Color::Yellow}));
 }
 
 TEST(GraphTest, threeItems) {
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Green, Edges{2}},
+                                      WithNode{Color::Yellow, Edges{0}}},
+                            ColorList{Color::Red}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Green, Edges{2}},
+                                      WithNode{Color::Yellow, Edges{0}}},
+                            ColorList{Color::Red, Color::Green}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Green, Edges{2}},
+                                      WithNode{Color::Yellow, Edges{0}}},
+                            ColorList{Color::Green, Color::Yellow}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Green, Edges{2}},
+                                      WithNode{Color::Yellow, Edges{0}}},
+                            ColorList{Color::Yellow, Color::Red}));
     EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{1}}, Node{Color::Green, Edges{2}},
-                       Node{Color::Yellow, Edges{0}}},
-                 ColorList{Color::Red}));
-    EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{1}}, Node{Color::Green, Edges{2}},
-                       Node{Color::Yellow, Edges{0}}},
-                 ColorList{Color::Red, Color::Green}));
-    EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{1}}, Node{Color::Green, Edges{2}},
-                       Node{Color::Yellow, Edges{0}}},
-                 ColorList{Color::Green, Color::Yellow}));
-    EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{1}}, Node{Color::Green, Edges{2}},
-                       Node{Color::Yellow, Edges{0}}},
-                 ColorList{Color::Yellow, Color::Red}));
-    EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{1}}, Node{Color::Green, Edges{2}},
-                       Node{Color::Yellow, Edges{0}}},
-                 ColorList{Color::Yellow, Color::Red, Color::Green}));
-    EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{1}}, Node{Color::Green, Edges{2}},
-                       Node{Color::Yellow, Edges{0}}},
-                 ColorList{Color::Red, Color::Yellow}));
-    EXPECT_FALSE(
-        has_path(Graph{Node{Color::Red, Edges{}}, Node{Color::Green, Edges{2}},
-                       Node{Color::Yellow, Edges{0}}},
-                 ColorList{Color::Red, Color::Yellow}));
+        testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                              WithNode{Color::Green, Edges{2}},
+                              WithNode{Color::Yellow, Edges{0}}},
+                    ColorList{Color::Yellow, Color::Red, Color::Green}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Green, Edges{2}},
+                                      WithNode{Color::Yellow, Edges{0}}},
+                            ColorList{Color::Red, Color::Yellow}));
+    EXPECT_FALSE(testHasPath(WithGraph{WithNode{Color::Red, Edges{}},
+                                       WithNode{Color::Green, Edges{2}},
+                                       WithNode{Color::Yellow, Edges{0}}},
+                             ColorList{Color::Red, Color::Yellow}));
 }
 
 TEST(GraphTest, bigGraph) {
+    EXPECT_TRUE(testHasPath(
+        WithGraph{
+            WithNode{Color::Red, Edges{1}}, WithNode{Color::Red, Edges{2}},
+            WithNode{Color::Red, Edges{3}}, WithNode{Color::Red, Edges{4}},
+            WithNode{Color::Red, Edges{5}}, WithNode{Color::Red, Edges{}}},
+        ColorList{Color::Red, Color::Red, Color::Red}));
     EXPECT_TRUE(
-        has_path(Graph{Node{Color::Red, Edges{1}}, Node{Color::Red, Edges{2}},
-                       Node{Color::Red, Edges{3}}, Node{Color::Red, Edges{4}},
-                       Node{Color::Red, Edges{5}}, Node{Color::Red, Edges{}}},
-                 ColorList{Color::Red, Color::Red, Color::Red}));
-    EXPECT_TRUE(has_path(Graph{Node{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Red, Edges{0, 1, 2, 3, 4, 5}}},
-                         ColorList{Color::Red, Color::Red, Color::Red}));
-    EXPECT_TRUE(has_path(Graph{Node{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Green, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Orange, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Yellow, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::Blue, Edges{0, 1, 2, 3, 4, 5}},
-                               Node{Color::White, Edges{0, 1, 2, 3, 4, 5}}},
-                         ColorList{Color::Red, Color::Yellow, Color::White}));
-
-    EXPECT_FALSE(has_path(Graph{Node{Color::Red, Edges{0, 1, 2, 3}},
-                                Node{Color::Green, Edges{0, 1, 2, 3}},
-                                Node{Color::Orange, Edges{0, 1, 2, 3}},
-                                Node{Color::Yellow, Edges{0, 1, 2, 3}},
-                                Node{Color::Blue, Edges{0, 1, 2, 3, 4, 5}},
-                                Node{Color::White, Edges{0, 1, 2, 3, 4, 5}}},
-                          ColorList{Color::Red, Color::Yellow, Color::White}));
-
-    EXPECT_TRUE(has_path(
-        Graph{Node{Color::Red, Edges{1}}, Node{Color::Green, Edges{2}},
-              Node{Color::Orange, Edges{3}}, Node{Color::Yellow, Edges{4}},
-              Node{Color::Blue, Edges{5}}, Node{Color::White, Edges{}}},
-        ColorList{Color::Red, Color::White}));
-
-    EXPECT_FALSE(has_path(
-        Graph{Node{Color::Red, Edges{1}}, Node{Color::Green, Edges{2}},
-              Node{Color::Orange, Edges{3}}, Node{Color::Yellow, Edges{4}},
-              Node{Color::Blue, Edges{5}}, Node{Color::White, Edges{}}},
+        testHasPath(WithGraph{WithNode{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Red, Edges{0, 1, 2, 3, 4, 5}}},
+                    ColorList{Color::Red, Color::Red, Color::Red}));
+    EXPECT_TRUE(
+        testHasPath(WithGraph{WithNode{Color::Red, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Green, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Orange, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Yellow, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::Blue, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::White, Edges{0, 1, 2, 3, 4, 5}}},
+                    ColorList{Color::Red, Color::Yellow, Color::White}));
+    EXPECT_FALSE(
+        testHasPath(WithGraph{WithNode{Color::Red, Edges{0, 1, 2, 3}},
+                              WithNode{Color::Green, Edges{0, 1, 2, 3}},
+                              WithNode{Color::Orange, Edges{0, 1, 2, 3}},
+                              WithNode{Color::Yellow, Edges{0, 1, 2, 3}},
+                              WithNode{Color::Blue, Edges{0, 1, 2, 3, 4, 5}},
+                              WithNode{Color::White, Edges{0, 1, 2, 3, 4, 5}}},
+                    ColorList{Color::Red, Color::Yellow, Color::White}));
+    EXPECT_TRUE(testHasPath(WithGraph{WithNode{Color::Red, Edges{1}},
+                                      WithNode{Color::Green, Edges{2}},
+                                      WithNode{Color::Orange, Edges{3}},
+                                      WithNode{Color::Yellow, Edges{4}},
+                                      WithNode{Color::Blue, Edges{5}},
+                                      WithNode{Color::White, Edges{}}},
+                            ColorList{Color::Red, Color::White}));
+    EXPECT_FALSE(testHasPath(
+        WithGraph{
+            WithNode{Color::Red, Edges{1}}, WithNode{Color::Green, Edges{2}},
+            WithNode{Color::Orange, Edges{3}},
+            WithNode{Color::Yellow, Edges{4}}, WithNode{Color::Blue, Edges{5}},
+            WithNode{Color::White, Edges{}}},
         ColorList{Color::Red, Color::White, Color::Orange}));
 }
 
+}  // namespace test
 }  // namespace graph
