a0a0a2ab481568740cdb04d321613fb794ff1097
Riccardo Ressi <riccardo.ressi@gmail.com>
2022-01-03 16:29:24 +0100

    dp::extractAllPermutations: support strings with non distinct chars

--- dynamic_programming/all_permutations.cpp
+++ dynamic_programming/all_permutations.cpp
@@ -1,57 +1,102 @@
 #include "all_permutations.h"
 
 #include <algorithm>
-#include <unordered_map>
+#include <cassert>
+#include <map>
+#include <vector>
 
 namespace dp {
 namespace {
 
 using CharacterSet = std::string;
 
-auto buildCharacterSet(const std::string &input) -> CharacterSet {
-    auto characters = CharacterSet(input);
-    std::sort(characters.begin(), characters.end());
-    auto it = std::unique(characters.begin(), characters.end());
-    characters.resize(std::distance(characters.begin(), it));
-    return characters;
+bool haveAllCharactersUnique(const std::string &input) {
+    auto charSet = std::set<char>(input.begin(), input.end());
+    return bool(charSet.size() == input.size());
 }
 
-using Memo = std::unordered_map<CharacterSet, AllPermutations>;
+template <class N>
+auto createFirstPermutation(N numItems) -> std::vector<N> {
+    auto permutation = std::vector<N>();
+    permutation.reserve(numItems);
 
-auto extractAllPermutationsRecursion(const CharacterSet &characterSet,
-                                     Memo *memo) -> const AllPermutations & {
-    auto memoIt = memo->find(characterSet);
+    for (auto i = N(); i < numItems; i++) {
+        permutation.emplace_back(i);
+    }
+
+    return permutation;
+}
+
+template <class Container>
+using Memo = std::map<Container, AllPermutations<Container>>;
+
+template <class Container>
+auto extractAllPermutationsRecursively(const Container &firstPermutation,
+                                       Memo<Container> *memo)
+    -> const AllPermutations<Container> & {
+    auto memoIt = memo->find(firstPermutation);
     if (memoIt != memo->end()) {
         return memoIt->second;
     }
 
-    auto allPermutations = AllPermutations();
-    if (characterSet.size() == 1) {
-        allPermutations.insert(characterSet);
-    } else if (characterSet.size() > 1) {
-        for (auto i = 0; i < characterSet.size(); i++) {
-            auto selectedChar = characterSet[i];
-            auto otherChars = characterSet.substr(0, i) +
-                              characterSet.substr(i + 1, std::string::npos);
-            for (const auto &permutation :
-                 extractAllPermutationsRecursion(otherChars, memo)) {
-                allPermutations.emplace(std::string{selectedChar} +
-                                        permutation);
+    auto allPermutations = AllPermutations<Container>();
+    if (firstPermutation.size() == 1) {
+        allPermutations.insert(firstPermutation);
+    } else if (firstPermutation.size() > 1) {
+        for (auto i = 0; i < firstPermutation.size(); i++) {
+            auto selectedItem = firstPermutation[i];
+            auto subPermutation = firstPermutation;
+            subPermutation.erase(subPermutation.begin() + i);
+            for (auto permutation :
+                 extractAllPermutationsRecursively(subPermutation, memo)) {
+                permutation.insert(permutation.begin(), selectedItem);
+                allPermutations.emplace(std::move(permutation));
             }
         }
     }
 
-    auto [it, _] = memo->emplace(characterSet, std::move(allPermutations));
+    auto [it, _] = memo->emplace(firstPermutation, std::move(allPermutations));
     return it->second;
+}
+
+template <class Permutation, class IndexPermutation>
+auto createPermutationFromIndices(const Permutation &firstPermutation,
+                                  const IndexPermutation &indexPermutation)
+    -> Permutation {
+    auto newPermutation = Permutation();
+    newPermutation.reserve(firstPermutation.size());
+
+    for (auto index : indexPermutation) {
+        assert(index < firstPermutation.size());
+        newPermutation.push_back(firstPermutation[index]);
+    }
+    return newPermutation;
 }
 
 }  // namespace
 
-auto extractAllPermutations(const std::string &input) -> AllPermutations {
-    auto characterSet = buildCharacterSet(input);
-    auto memo = Memo();
-    extractAllPermutationsRecursion(characterSet, &memo);
-    return std::move(memo.at(characterSet));
+auto extractAllPermutations(const std::string &input)
+    -> AllPermutations<std::string> {
+    if (haveAllCharactersUnique(input)) {
+        auto memo = Memo<std::string>();
+        extractAllPermutationsRecursively(input, &memo);
+        return std::move(memo.at(input));
+    }
+
+    auto firstPermutation = createFirstPermutation(input.size());
+
+    using Index = std::string::size_type;
+    auto memo = Memo<std::vector<Index>>();
+    const auto &indexPermutations =
+        extractAllPermutationsRecursively(firstPermutation, &memo);
+
+    auto allPermutations = AllPermutations<std::string>();
+    for (const auto &indexPermutation : indexPermutations) {
+        allPermutations.emplace(
+            createPermutationFromIndices(input, indexPermutation));
+    }
+
+    return allPermutations;
 }
 
 }  // namespace dp
--- dynamic_programming/all_permutations.t.cpp
+++ dynamic_programming/all_permutations.t.cpp
@@ -9,10 +9,13 @@ namespace {
 
 using NumItems = std::size_t;
 using NumExpectedPermutations = std::size_t;
-using ExpectedOutcome = std::unordered_set<std::string>;
+using ExpectedOutcome = AllPermutations<std::string>;
+using DinstinctChars = bool;
 
 struct TestCase {
     NumItems numItems{};
+    DinstinctChars distrinctChars{};
+
     NumExpectedPermutations numExpectedPermuations{};
     ExpectedOutcome expectedOutcome{};
 };
@@ -29,9 +32,17 @@ TEST_P(TestAllPermutations, testExtractAllPermutations) {
     const auto &param = TestAllPermutations::GetParam();
 
     auto input = std::string();
-    input.reserve(param.numItems);
-    for (auto i = NumItems(0); i < param.numItems; i++) {
-        input.push_back('a' + i);
+    if (param.distrinctChars) {
+        input.reserve(param.numItems);
+        for (auto i = NumItems(0); i < param.numItems; i++) {
+            input.push_back('a' + i);
+        }
+    } else {
+        auto moduloFactor =
+            std::max<NumItems>(1, (param.numItems / 2) + (param.numItems & 1));
+        for (auto i = NumItems(0); i < param.numItems; i++) {
+            input.push_back('a' + (i % moduloFactor));
+        }
     }
     ASSERT_EQ(param.numItems, input.size());
 
@@ -42,42 +53,100 @@ TEST_P(TestAllPermutations, testExtractAllPermutations) {
     }
 }
 
-INSTANTIATE_TEST_SUITE_P(ZeroToThree, TestAllPermutations,
-                         testing::Values(
-
-                             TestCase{NumItems(0),                 //
-                                      NumExpectedPermutations(0),  //
-                                      ExpectedOutcome{}},
-                             TestCase{NumItems(1),                 //
-                                      NumExpectedPermutations(1),  //
-                                      ExpectedOutcome{"a"}},
-                             TestCase{NumItems(2),                 //
-                                      NumExpectedPermutations(2),  //
-                                      ExpectedOutcome{"ab", "ba"}},
-                             TestCase{NumItems(3),                 //
-                                      NumExpectedPermutations(6),  //
-                                      ExpectedOutcome{"abc", "acb", "bac",
-                                                      "bca", "cab", "cba"}}
-
-                             ),
-                         &TestAllPermutations::getTestName);
-
 INSTANTIATE_TEST_SUITE_P(
-    FourToTen, TestAllPermutations,
+    ZeroToThree, TestAllPermutations,
     testing::Values(
 
-        TestCase{NumItems(4), NumExpectedPermutations(24)},
-        TestCase{NumItems(5), NumExpectedPermutations(120)},
-        TestCase{NumItems(6), NumExpectedPermutations(720)},
-        TestCase{NumItems(7), NumExpectedPermutations(5'040)},
-        TestCase{NumItems(8), NumExpectedPermutations(40'320)}
+        TestCase{NumItems(0), DinstinctChars{true},
+
+                 NumExpectedPermutations(0), ExpectedOutcome{}},
+
+        TestCase{NumItems(1), DinstinctChars{true},
+
+                 NumExpectedPermutations(1), ExpectedOutcome{"a"}},
+
+        TestCase{NumItems(2), DinstinctChars{true},
+
+                 NumExpectedPermutations(2), ExpectedOutcome{"ab", "ba"}},
+
+        TestCase{NumItems(3), DinstinctChars{true},
+
+                 NumExpectedPermutations(6),
+                 ExpectedOutcome{"abc", "acb", "bac", "bca", "cab", "cba"}},
+
+        TestCase{NumItems(0), DinstinctChars{false},
+
+                 NumExpectedPermutations(0), ExpectedOutcome{}},
+
+        TestCase{NumItems(1), DinstinctChars{false},
+
+                 NumExpectedPermutations(1), ExpectedOutcome{"a"}},
+
+        TestCase{NumItems(2), DinstinctChars{false},
+
+                 NumExpectedPermutations(1), ExpectedOutcome{"aa"}},
+
+        TestCase{NumItems(3), DinstinctChars{false},
+
+                 NumExpectedPermutations(3),
+                 ExpectedOutcome{"aab", "aba", "baa"}}
 
         ),
     &TestAllPermutations::getTestName);
 
+INSTANTIATE_TEST_SUITE_P(FourToTen, TestAllPermutations,
+                         testing::Values(
+
+                             TestCase{NumItems(4), DinstinctChars{true},
+
+                                      NumExpectedPermutations(24)},
+
+                             TestCase{NumItems(5), DinstinctChars{true},
+
+                                      NumExpectedPermutations(120)},
+
+                             TestCase{NumItems(6), DinstinctChars{true},
+
+                                      NumExpectedPermutations(720)},
+
+                             TestCase{NumItems(7), DinstinctChars{true},
+
+                                      NumExpectedPermutations(5'040)},
+
+                             TestCase{NumItems(8), DinstinctChars{true},
+
+                                      NumExpectedPermutations(40'320)},
+
+                             TestCase{NumItems(4), DinstinctChars{false},
+
+                                      NumExpectedPermutations(6)},
+
+                             TestCase{NumItems(5), DinstinctChars{false},
+
+                                      NumExpectedPermutations(30)},
+
+                             TestCase{NumItems(6), DinstinctChars{false},
+
+                                      NumExpectedPermutations(90)},
+
+                             TestCase{NumItems(7), DinstinctChars{false},
+
+                                      NumExpectedPermutations(630)},
+
+                             TestCase{NumItems(8), DinstinctChars{false},
+
+                                      NumExpectedPermutations(2'520)}
+
+                             ),
+                         &TestAllPermutations::getTestName);
+
 auto TestAllPermutations::getTestName(
     const ::testing::TestParamInfo<TestCase> &testInfo) -> std::string {
-    return "n_" + std::to_string(testInfo.param.numItems);
+    auto testName = "n_" + std::to_string(testInfo.param.numItems);
+    if (testInfo.param.distrinctChars) {
+        testName.append("_distrinctChars");
+    }
+    return testName;
 }
 
 }  // namespace dp
