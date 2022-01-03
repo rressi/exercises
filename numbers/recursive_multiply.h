9fb445dc93ddf6e59a15bf465e3482d4b5334ba0
Riccardo Ressi <riccardo.ressi@gmail.com>
2022-01-03 12:03:38 +0100

    recursiveMultiply implemented

---
+++ numbers/recursive_multiply.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+
+namespace numbers {
+
+using BinStringView = std::string_view;
+using HexStringView = std::string_view;
+
+bool matchHexAndBin(HexStringView hex, BinStringView bin);
+
+auto readBinDigits(BinStringView bin, unsigned n)
+    -> std::tuple<unsigned, BinStringView>;
+
+auto readHexDigit(HexStringView hex) -> std::tuple<unsigned, HexStringView>;
+
+auto decodeBinDigit(char digit) -> unsigned;
+
+auto decodeHexDigit(char digit) -> unsigned;
+
+}  // namespace numbers
